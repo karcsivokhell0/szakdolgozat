29e8ecdd0f6c32fae794bc9904f74549b0c25e4e
karcsivokhell0 <zolyomikaresz06@gmail.com>
2024-10-22 16:21:46 +0200

    laravel init

--- config/cache.php
+++ config/cache.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('CACHE_STORE', 'database'),
+    'default' => env('CACHE_STORE', 'array'),
 
     /*
     |--------------------------------------------------------------------------
@@ -33,6 +33,9 @@ return [
 
     'stores' => [
 
+        'none' => [
+            'driver' => 'null',
+        ],
         'array' => [
             'driver' => 'array',
             'serialize' => false,
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BaseController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [BaseController::class, "welcome"]);
