17c2c32a40f38b7c7dce570550cdcb7a0df5d8dc
karcsivokhell0 <zolyomikaresz06@gmail.com>
2024-11-10 19:23:06 +0100

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('css/app.css')}}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
@@ -34,12 +35,11 @@
 </nav>
 
 <br><br><br><br>
-<div class="container-fluid bg-info-subtle">
-    <div class="container">
-        <div class="row">
-            <h1 style="text-align:center;">Websites for everyone</h1>
-        </div>
-    </div>
+<div class="container-fluid">
+  <div class="container">
+    <h1 class="col-3 cigany">Website for everyone</h1>
+
+  </div>
 </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BaseController;
 
-Route::get('/', [BaseController::class, "welcome"]);
-Route::get('/home', [BaseController::class, "home"]);
+
+Route::get('/', [BaseController::class, "home"]);
