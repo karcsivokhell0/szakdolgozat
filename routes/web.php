562af890029317414261bc074e510d8695fd2c54
karcsivokhell0 <zolyomikaresz06@gmail.com>
2024-11-14 18:45:36 +0100

    buzi GAla

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BaseController;
 
 
-Route::get('/', [BaseController::class, "home"]);
+Route::get('/home', [BaseController::class, "home"]);
