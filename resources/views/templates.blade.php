1709f2c2e40f48522b0632f8a1eec8cd6ed1bcf8
Karcsivokhello <zolyomikaresz06@gmail.com>
2025-04-15 18:24:11 +0200

    footer :c

--- resources/views/templates.blade.php
+++ resources/views/templates.blade.php
@@ -90,4 +90,13 @@
     @endguest
 </body>
 
+<footer class="py-3">
+  <ul class="nav justify-content-center pb-3 mb-3">
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
+    <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
+  </ul>
+  <p class="text-center text-body-secondary">© 2024 Company, Inc</p>
+</footer>
+
 </html>
